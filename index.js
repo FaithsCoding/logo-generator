fda77ebbeb00c46c5a21f889a1f4e38d614948a3
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-04 12:49:09 +0100

    corrected the shapes const so it now creates the file

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./utils/shapes');
+const { Square, Circle, Triangle }  = require('./utils/shapes');
 
-class Svg{
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
+class Svg {
+    constructor() {
+        this.textElement = '';
+        this.shapeElement = '';
     }
-    render(){
-
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
     }
-    setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
     }
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
-
+    setShapeElement(shape) {
+        this.shapeElement = shape.render();
     }
-    
 }
 
 const questions = [
     {
-      type: "input",
-      name: "text",
-      message: "Text: Enter up to 3 characters:",  
+        type: 'input',
+        name: 'text',
+        message: 'Text: Enter up to 3 characters:',
     },
     {
-        type: "input",
-        name: "text-color",
-        message: "Text color: Enter a color keyword:",
+        type: 'input',
+        name: 'text-color',
+        message: 'Text color: Enter a color keyword:',
     },
     {
-        type: "input",
-        name: "shape-color",
-        messagee: "Shape color: Enter a color keyword",
+        type: 'input',
+        name: 'shape-color',
+        message: 'Shape color: Enter a color keyword',
     },
     {
-        type: "list",
-        name: "shape",
-        message: "Choose which shape you would like",
-        choices: ["Circle", "Square", "Triange"],
-    }
-]
+        type: 'list',
+        name: 'shape',
+        message: 'Choose which shape you would like',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+];
 
 function writeToFile(fileName, data) {
-    filesystem.writeFile(fileName, data, function (err) {
+    fs.writeFile(fileName, data, function (err) {
         if (err) {
             return console.log(err);
         }
@@ -54,35 +51,32 @@ function writeToFile(fileName, data) {
 }
 
 async function init() {
-    var svgString = "";
-    var svg_file = "logo.svg";
+    var svgString = '';
+    var svg_file = 'logo.svg';
 
     const answers = await inquirer.prompt(questions);
 
-    var user_text = "";
+    var user_text = '';
     if (answers.text.length > 0 && answers.text.length < 4) {
         user_text = answers.text;
     } else {
-        console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
+        console.log('Invalid user text field detected! Please enter 1-3 Characters, no more and no less');
         return;
     }
 
-    user_font_color = answers["text-color"];
-    user_shape_color = answers.shape;
-    user_shape_type = answers["shape"];
-    
+    user_font_color = answers['text-color'];
+    user_shape_color = answers['shape-color'];
+    user_shape_type = answers['shape'];
+
     let user_shape;
-    if (user_shape_type === "Square" || user_shape_type === "square") {
+    if (user_shape_type === 'Square' || user_shape_type === 'square') {
         user_shape = new Square();
-    }
-    else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+    } else if (user_shape_type === 'Circle' || user_shape_type === 'circle') {
         user_shape = new Circle();
-    }
-    else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+    } else if (user_shape_type === 'Triangle' || user_shape_type === 'triangle') {
         user_shape = new Triangle();
-    }
-    else {
-        console.log("Invalid shape!");
+    } else {
+        console.log('Invalid shape!');
     }
     user_shape.setColor(user_shape_color);
 
@@ -90,7 +84,7 @@ async function init() {
     svg.setTextElement(user_text, user_font_color);
     svg.setShapeElement(user_shape);
     svgString = svg.render();
-    
-    writeToFile(svg_file, svgString); 
+
+    writeToFile(svg_file, svgString);
 }
-init()
+init();
